ac92599ddf5ba5b0b987c0192867aa9e2f4d61cf
FlorianZasada <florian.zasada@gmail.com>
2022-12-09 10:49:23 +0100

    init

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueApexCharts from 'vue3-apexcharts'
 
-createApp(App).use(store).use(router).mount('#app')
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+createApp(App).use(store).use(router).use(VueApexCharts).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home'
+import BotView from '../views/BotView'
+import AddBot from '../views/AddBot'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/bot/:name',
+    name: 'BotView',
+    component: BotView,
+    props: true
+  },
+  {
+    path: '/bot/add',
+    name: 'AddBot',
+    component: AddBot,
+  },
 ]
 
 const router = createRouter({
